43fad6c7059969c5258666fdb53d0034bbf19f0d
Andrew Cer <cer.andrew@gmail.com>
2015-08-24 14:31:49 -0600

    added error handing of get request

--- js/controller.js
+++ js/controller.js
@@ -5,3 +5,33 @@ app.controller('HomeController', function ($scope) {
     $scope.textToKebab = $scope.userInput
   }
 })
+
+
+app.controller('ServiceController', function ($scope, $http) {
+  // $http.get('https://api.github.com/zen')
+  // .then(function (data) {
+  //   $scope.zenWis = data.data
+  // })
+
+  //!!!!!!!
+  //error handeling of get request
+  //!!!!
+  $http.get('https://itunes.apple.com/search?term=jack+johnson')
+  .then(function(response) {
+        console.log('get',response)
+    },
+    function(data) {
+        if (data.status == 0) {
+          $scope.errors = 'Whoops, something went wrong'
+        }
+    })
+  // $http.get('/itunes.json')
+  // .then(function (data) {
+  //   var holderArray = [];
+  //   console.log(data.data.results);
+  //   for (var i = 0; i < data.data.results.length; i++) {
+  //     holderArray.push(data.data.results[i].collectionName)
+  //   }
+  //   $scope.titles = holderArray
+  // })
+})
